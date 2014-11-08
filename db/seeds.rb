bf74a9455db307a70e2592e9d9b1077a812860e9
Robbie Clutton <robert.clutton@gmail.com>
2014-11-08 22:23:25 +0000

    Basic seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,27 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+(1..10000).each do |i|
+  part = Part.create(name: "part_#{i}", cost: rand(1000))
+  instance_variable_set "@part_#{i}", part
+end
+
+(1..1000).each do |i|
+  component = Component.create(name: "component_#{i}", manufacture_cost: rand(10000))
+  instance_variable_set "@component_#{i}", component
+  lower = (i - 1) * 10 + 1
+  upper = lower + 99
+  (lower..upper).each do |j|
+    part = instance_variable_get("@part_#{j}")
+    ComponentPart.create component: component, part: part
+  end
+end
+
+(1..10).each do |i|
+  product = Product.create name: "product_#{i}", manufacture_cost: rand(100000)
+  lower = (i - 1) * 10 + 1
+  upper = lower + 99
+  (lower..upper).each do |j|
+    component = instance_variable_get("@component_#{j}")
+    ProductComponent.create product: product, component: component
+  end
+
+end
+
