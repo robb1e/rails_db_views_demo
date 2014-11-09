9feede5f71e7fde2bd1d13be8bdd066856155961
Robbie Clutton <robert.clutton@gmail.com>
2014-11-09 21:08:51 +0000

    Use Postgres

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141108212944) do
+ActiveRecord::Schema.define(version: 20141109083225) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
 
   create_table "component_parts", force: true do |t|
     t.integer "component_id"
